9f553238c9a38368ef86cec53732689f9e2fcc80
Lucas Maciel <farias.maciel@outlook.com>
2023-07-25 00:18:24 -0400

    Algumas correções e melhorias no desafio proposto

--- 7_TEMPLATE_ENGINE/10_desafio/index.js
+++ 7_TEMPLATE_ENGINE/10_desafio/index.js
@@ -12,35 +12,43 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    const products = [
-        {
-            name: 'TV',
-            price: '3.999,00',
-            description: 'Televisao 55 polegadas',
-            link: '/televisao'
-        },
-        {
-            name: 'Smartphone',
-            price: '1.800,00',
-            description: 'Smartphone android marca x',
-            link: '/smartphone'
-        },
-        {
-            name: 'Console ultraX',
-            price: '3.500,00',
-            description: 'Console de ultima geração',
-            link: '/ultrax'
-        },
-        {
-            name: 'Blusa Azul',
-            price: '52,00',
-            description: 'Blusa azul tamanhos variados',
-            link: '/blusa'
-        }
-    ]
+const products = [
+    {
+        name: 'TV',
+        price: '3.999,00',
+        description: 'Televisao 55 polegadas',
+        id: '1'
+    },
+    {
+        name: 'Smartphone',
+        price: '1.800,00',
+        description: 'Smartphone android marca x',
+        id: '2'
+    },
+    {
+        name: 'Console ultraX',
+        price: '3.500,00',
+        description: 'Console de ultima geração',
+        id: '3'
+    },
+    {
+        name: 'Blusa Azul',
+        price: '52,00',
+        description: 'Blusa azul tamanhos variados',
+        id: '4'
+    }
+]
 
+app.get('/', (req, res) => {
     res.render('home', {products})
+})
+
+app.get('/product/:id', (req, res) => {
+    const product = products[parseInt(req.params.id) - 1]
+
+    console.log(`Acessando a pagina do produto ${product.name}`)
+    
+    res.render('pageproduct', {product})
 })
 
 app.listen(port, () => {
