aed133787dec97e7afdb7d65e697c3189b3e16a5
Lucas Maciel <farias.maciel@outlook.com>
2023-07-29 22:11:39 -0400

    Update de dados, preenchedo form para atualizar os dados

---
+++ 8_NODE_E_MYSQL/5_edicao_primeira_etapa/index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sql = `INSERT INTO books(title, pageqty) VALUES('${title}', '${pageqty}')`
+
+    conn.query(sql, function(err) {
+        if(err){
+            console.log(err)
+            return
+        }
+        console.log('Livro adicionado com sucesso!')
+        res.redirect('/books')
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/books', (req, res) => {
+    const sql = 'SELECT * FROM books'
+
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+        }
+        const books = data
+        console.log(data)
+        res.render('books', {books})
+    })
+})
+
+app.get('/books/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `SELECT * FROM books WHERE id= ${id}` 
+
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+        
+        res.render('book', {book})
+    })
+})
+
+app.get('/books/edit/:id', (req, res) => {
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+        }
+
+        const book = data[0]
+        
+        res.render('editbook', {book})
+
+    })
+})
+
+const conn = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'nodemysql'
+})
+
+conn.connect(function(err){
+    if(err){
+        console.log(err)
+        return
+    }
+
+    console.log('Conectado ao banco com sucesso')
+    
+    app.listen(port, () => console.log(`Listen in port ${port}`))
+})
