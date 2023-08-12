9078932332ca15ccfeea3cb05be8dd66a62a4053
Lucas Maciel <farias.maciel@outlook.com>
2023-08-11 23:09:34 -0400

    removendo dados relacionados

--- 9_SEQUELIZE/12_remover_relacionados/index.js
+++ 9_SEQUELIZE/12_remover_relacionados/index.js
@@ -3,8 +3,9 @@ const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 const User = require('./model/User')
 const Address = require('./model/Address')
-const port = 3000
+
 const app = express()
+const port = 3000
 
 app.use(express.static('public'))
 app.use(express.urlencoded({
@@ -23,14 +24,14 @@ app.get('/users/create', (req, res) => {
     res.render('adduser')
 })
 
-app.post('/users/create', async(req, res) => {
+app.post('/users/create', async (req, res) => {
     const name = req.body.name
     const occupation = req.body.occupation
     let newsletter = req.body.newsletter
 
-    if(newsletter === 'on'){
+    if(newsletter === 'on') {
         newsletter = true
-    } else {
+    } else{
         newsletter = false
     }
 
@@ -45,15 +46,15 @@ app.get('/', async (req, res) => {
     res.render('home', {users})
 })
 
-app.get('/users/:id', async(req, res) => {
+app.get('/users/:id', async (req, res) => {
     const id = req.params.id
 
     const user = await User.findOne({raw: true, where: {id}})
-    
+
     res.render('userview', {user})
 })
 
-app.get('/users/edit/:id', async(req, res) => {
+app.get('/users/edit/:id', async (req, res) => {
     const id = req.params.id
 
     try {
@@ -64,11 +65,11 @@ app.get('/users/edit/:id', async(req, res) => {
     }
 })
 
-app.post('/users/update', async(req, res) => {
+app.post('/users/update', async (req, res) => {
+    const id = req.body.id
     const name = req.body.name
     const occupation = req.body.occupation
     let newsletter = req.body.newsletter
-    const id = req.body.id
 
     if(newsletter === 'on'){
         newsletter = true
@@ -77,13 +78,13 @@ app.post('/users/update', async(req, res) => {
     }
 
     const userData = {id, name, occupation, newsletter}
-
+    
     await User.update(userData, {where: {id}})
 
     res.redirect('/')
 })
 
-app.post('/users/remove/:id', async(req, res) => {
+app.post('/users/remove/:id', async (req, res) => {
     const id = req.params.id
 
     await User.destroy({where: {id}})
@@ -100,6 +101,41 @@ app.post('/address/create', async (req, res) => {
     const addressData = {UserId, street, number, city}
 
     await Address.create(addressData)
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
+app.post('/address/delete', async (req, res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+
+    await Address.destroy({where: {id}})
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
+app.get('/address/edit/:id', async(req, res) => {
+
+    const id = req.params.id
+
+    try {
+        const address = await Address.findOne({where: {id}})
+        res.render('addressedit', {address: address.get({plain: true})})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post('/address/update', async (req, res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+
+    const addressData = {UserId, id, street, number, city}
+
+    await Address.update(addressData, {where: {id}})
 
     res.redirect(`/users/edit/${UserId}`)
 })
