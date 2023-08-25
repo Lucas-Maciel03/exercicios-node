4496c9848a88e250b3ff1af97f207ea1efabff6e
Lucas Maciel <farias.maciel@outlook.com>
2023-08-24 23:18:49 -0400

    adicionando novas funções em toughts routes e toughts controller

--- 11_TOUGHTS/routes/toughtsRoutes.js
+++ 11_TOUGHTS/routes/toughtsRoutes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const toughtController = require('../controllers/ToughtController')
 
+//helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add', checkAuth, toughtController.createTought)
+router.post('/add', checkAuth, toughtController.createToughtSave)
+router.get('/dashboard', checkAuth, toughtController.dashboard)
 router.get('/', toughtController.showToughts)
 
 module.exports = router
