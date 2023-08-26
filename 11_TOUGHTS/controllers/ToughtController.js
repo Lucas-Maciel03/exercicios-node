f633824ba0c89e35cafd67fe7a037e9b83061a57
Lucas Maciel <farias.maciel@outlook.com>
2023-08-25 22:29:59 -0400

    delete e update de toughts

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -3,12 +3,36 @@ const User = require('../models/User')
 
 module.exports = class ToughtController {
     static async showToughts(req, res){
+
         res.render('toughts/home')
     }
 
     static async dashboard(req, res){
+        const userId = req.session.userid
+
+        const user = await User.findOne({
+            where: {id: userId
+            },
+            include: Tought,
+            plain: true
+        })
+
+        //check if user exists
+        if(!user){
+            res.redirect('/login')
+        }
+        //map() pode iterar e modificar os itens de um array
+        const toughts = user.Toughts.map((result) => result.dataValues)
+        /*result = cada um dos itens. A função acime está cortando tudo 
+        e informando q o resultado é apenas o que tem em dataValues*/ 
         
-        res.render('toughts/dashboard', {toughts})
+        let emptyToughts = false
+
+        if(toughts.length === 0){
+            emptyToughts = true
+        }
+
+        res.render('toughts/dashboard', {toughts, emptyToughts})
     }
 
     static createTought(req, res){
@@ -30,6 +54,52 @@ module.exports = class ToughtController {
                 res.redirect('/toughts/dashboard')
             })
 
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static async removeTought(req, res){
+        const id = req.body.id
+        const UserId = req.session.userid 
+
+        //para verificar se o proprio usuario quer remover seu pensamento
+        try {
+            await Tought.destroy({where: {id: id, UserId: UserId}})
+
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static async updateTought(req, res){
+        const id = req.params.id
+
+        const tought = await Tought.findOne({where: {id}, raw: true})
+
+        res.render('toughts/update', {tought})
+    }
+
+    static async updateToughtSave(req, res){
+        const id = req.body.id
+
+        const tought = {
+            title: req.body.title
+        }
+
+        try {
+            await Tought.update(tought, {where: {id}})
+
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
         } catch (error) {
             console.log(error)
         }
