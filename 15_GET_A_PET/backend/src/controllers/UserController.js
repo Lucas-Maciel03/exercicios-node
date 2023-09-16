db5a24b3bc1efdb5a5f990852ee7b21643f87c40
Lucas Maciel <farias.maciel@outlook.com>
2023-09-15 23:02:00 -0400

    validacoes no register de user

--- 15_GET_A_PET/backend/src/controllers/UserController.js
+++ 15_GET_A_PET/backend/src/controllers/UserController.js
@@ -2,6 +2,47 @@ const User = require('../models/User')
 
 module.exports = class UserController{
     static async register(req, res){
-        res.json('Olá Get a Pet')
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        //validations
+        if(!name){
+            res.status(422).json({ message: 'O nome é obrigatório' })
+            return
+        }
+
+        if(!email){
+            res.status(422).json({ message: 'O e-mail é obrigatório' })
+            return
+        }
+
+        if(!phone){
+            res.status(422).json({ message: 'O número de telefone é obrigatório' })
+            return
+        }
+
+        if(!password){
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+
+        if(!confirmpassword){
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória' })
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({ message: 'As senhas não conferem' })
+            return
+        }
+
+        //check if user exists
+        const userExists = await User.findOne({email: email})
+
+        if(userExists){
+            res.status(422).json({ message: 'O e-mail já está cadastrado' })
+            return
+        }
+
+        res.status(200).json({ message: 'ok' })
     } 
 }
