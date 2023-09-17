aeeb6abdb541d0c87e2263c64ba38ea6f8c0d745
Lucas Maciel <farias.maciel@outlook.com>
2023-09-16 22:15:01 -0400

    funcionalidade login

--- 15_GET_A_PET/backend/src/controllers/UserController.js
+++ 15_GET_A_PET/backend/src/controllers/UserController.js
@@ -64,7 +64,40 @@ module.exports = class UserController{
 
             await createUserToken(newUser, req, res)
         } catch (error) {
-            res.status(500).json({message: error})
+            res.status(500).json({ message: error })
         }
     }
+
+    static async login(req, res){
+        const {email, password} = req.body
+
+        if(!email){
+            res.status(422).json({ message: 'O e-mail é obrigatório' })
+            return
+        }
+
+        if(!password){
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+
+        //check if user exists
+        const user = await User.findOne({ email: email })
+        
+        if(!user){
+            res.status(422).json({ message: 'O e-mail não está cadastrado' })
+            return
+        }
+
+        //check if password match
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword){
+            res.status(422).json({ message: 'Senha inválida' })
+            return
+        }
+        
+        await createUserToken(user, req, res)
+
+    } 
 }
