7f7dc0401af1ac8fbac7762e91bae595e04a3e55
Lucas Maciel <farias.maciel@outlook.com>
2023-09-18 23:04:39 -0400

    criando rota de atualização de usuario

--- 15_GET_A_PET/backend/src/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/src/routes/UserRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router()
 
 const UserController = require('../controllers/UserController')
 
+//middlewares
+const verifyToken = require('../middlewares/verify-token')
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 module.exports = router
