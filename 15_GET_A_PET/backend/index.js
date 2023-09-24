0713e9fd169e5f34c238dfd43d23a5d22fdb9eb0
Lucas Maciel <farias.maciel@outlook.com>
2023-09-23 20:40:57 -0400

    criando e config rotas de pets

--- 15_GET_A_PET/backend/index.js
+++ 15_GET_A_PET/backend/index.js
@@ -15,7 +15,8 @@ app.use(express.static('public'))
 
 // Routes
 const userRoutes = require('./src/routes/userRoutes')
+const petRoutes = require('./src/routes/petRoutes')
 app.use('/users', userRoutes)
-
+app.use('/pets', petRoutes)
 
 app.listen(3000, () => console.log('Listen in port 3000'))
